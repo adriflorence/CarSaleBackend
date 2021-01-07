be3d4a0609ce0dabde1bf377fbc85c1661af7cb2
Adri Forczek <forczekadri@gmail.com>
2021-01-07 23:12:50 +0000

    feature: implements put and delete mappings

--- vehicle-api/src/main/java/com/adriforczek/vehicles/api/CarController.java
+++ vehicle-api/src/main/java/com/adriforczek/vehicles/api/CarController.java
@@ -5,6 +5,7 @@ import com.adriforczek.vehicles.domain.car.Car;
 import com.adriforczek.vehicles.service.CarService;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -61,7 +62,7 @@ public class CarController {
      * @return response that the new vehicle was added to the system
      * @throws URISyntaxException if the request contains invalid fields or syntax
      */
-    @PostMapping
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<?> post(@Valid @RequestBody Car car) throws URISyntaxException {
         carService.save(car);
         Resource<Car> resource = assembler.toResource(car);
@@ -76,13 +77,9 @@ public class CarController {
      */
     @PutMapping("/{id}")
     ResponseEntity<?> put(@PathVariable Long id, @Valid @RequestBody Car car) {
-        /**
-         * TODO: Set the id of the input car object to the `id` input.
-         * TODO: Save the car using the `save` method from the Car service
-         * TODO: Use the `assembler` on that updated car and return as part of the response.
-         *   Update the first line as part of the above implementing.
-         */
-        Resource<Car> resource = assembler.toResource(new Car());
+        car.setId(id);
+        carService.save(car);
+        Resource<Car> resource = assembler.toResource(car);
         return ResponseEntity.ok(resource);
     }
 
@@ -93,9 +90,7 @@ public class CarController {
      */
     @DeleteMapping("/{id}")
     ResponseEntity<?> delete(@PathVariable Long id) {
-        /**
-         * TODO: Use the Car Service to delete the requested vehicle.
-         */
+        carService.delete(id);
         return ResponseEntity.noContent().build();
     }
 }
